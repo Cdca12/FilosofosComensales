57ce49ce364b86156568a1e7f23f82f153dfc88d
Carlos Contreras <cdca12mx@gmail.com>
2019-04-25 16:11:11 -0600

    Creación del proyecto

---
+++ src/filosofoscomensales/Main.java
@@ -0,0 +1,13 @@
+package filosofoscomensales;
+
+/**
+ *
+ * @author Carlos Contreras
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
