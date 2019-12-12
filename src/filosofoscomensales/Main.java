3de2aa8d62becb9350825bebba457c5f79e0e80d
Carlos Contreras <cdca12mx@gmail.com>
2019-12-12 15:52:50 -0700

    Proyecto terminado

--- src/filosofoscomensales/Main.java
+++ src/filosofoscomensales/Main.java
@@ -7,7 +7,7 @@ package filosofoscomensales;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        new FilosofosSimulacion();
     }
     
 }
